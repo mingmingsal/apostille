9009803783dc3123572a7a618275f4ebf6d9f47d
mingmingsal <miguellorenzosalvador@gmail.com>
2024-05-19 10:00:58 +0800

    enlarged country title

--- src/components/CountryDisplay.jsx
+++ src/components/CountryDisplay.jsx
@@ -10,9 +10,9 @@ const CountryDisplay = ({ country }) => {
     goToAgency.current = goToAgency.current.slice(0, country.national_agencies.length);
   }, [country.lgu, country.national_agencies]);
 
-  function jumpTo(e) {
-    if (goToLGU.current[e.target.value] === undefined) return;
-    goToLGU.current[e.target.value].scrollIntoView(true);
+  function jumpTo(e, goTo) {
+    if (goTo.current[e.target.value] === undefined) return;
+    goTo.current[e.target.value].scrollIntoView(true);
   }
   function CheckifValid({ e, children }) {
     if (e == null) return null;
@@ -20,7 +20,7 @@ const CountryDisplay = ({ country }) => {
   }
   return (
     <div className="country">
-      <h2>{country.name}</h2>
+      <h2 className="countryTitle">{country.name}</h2>
 
       <CheckifValid e={country.national_agencies}>
         <h3>National Agencies</h3>
@@ -31,7 +31,7 @@ const CountryDisplay = ({ country }) => {
         <h3>Local Governments</h3>
         <ChooseElement
           countries={country.lgu}
-          callbackFn={jumpTo}
+          callbackFn={(e)=>jumpTo(e,goToLGU)}
           flavorText="Choose LGU..."
         />
         <RenderList countryPart={country.lgu} country={country} goTo={goToLGU}/>
