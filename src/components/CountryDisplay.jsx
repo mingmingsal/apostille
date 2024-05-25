3d39d8eb6b1522386c34d237e01eb80025c260bb
mingmingsal <miguellorenzosalvador@gmail.com>
2024-05-25 18:27:43 +0800

    fixed sort

--- src/components/CountryDisplay.jsx
+++ src/components/CountryDisplay.jsx
@@ -11,32 +11,42 @@ const CountryDisplay = ({ country }) => {
     goToLGU.current = goToLGU.current.slice(0, country.lgu.length);
 
     if (country.national_agencies == null) return;
-    goToAgency.current = goToAgency.current.slice(0, country.national_agencies.length);
-
+    goToAgency.current = goToAgency.current.slice(
+      0,
+      country.national_agencies.length
+    );
   }, [country.lgu, country.national_agencies]);
 
   function jumpTo(e, goTo) {
     if (goTo.current[e.target.value] === undefined) return;
     goTo.current[e.target.value].scrollIntoView(true);
   }
-  
+
   return (
     <div className="country">
       <h2 className="countryTitle">{country.name}</h2>
 
       <CheckifValid e={country.national_agencies}>
         <h3>National Agencies</h3>
-        <RenderList countryPart={country.national_agencies} country={country} goTo={goToAgency} />
+        <RenderList
+          countryPart={country.national_agencies}
+          country={country}
+          goTo={goToAgency}
+        />
       </CheckifValid>
 
       <CheckifValid e={country.lgu}>
         <h3>Local Governments</h3>
         <ChooseElement
           countries={country.lgu}
-          callbackFn={(e)=>jumpTo(e,goToLGU)}
+          callbackFn={(e) => jumpTo(e, goToLGU)}
           flavorText="Choose LGU..."
         />
-        <RenderList countryPart={country.lgu} country={country} goTo={goToLGU}/>
+        <RenderList
+          countryPart={country.lgu}
+          country={country}
+          goTo={goToLGU}
+        />
       </CheckifValid>
     </div>
   );
